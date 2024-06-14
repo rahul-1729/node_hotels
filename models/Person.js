1b630590ea4d6f03c380b5dd104126f0845312b6
rahul-1729 <rk9763981@gmail.com>
2024-06-14 07:15:42 +0530

    authentication

--- models/Person.js
+++ models/Person.js
@@ -26,6 +26,14 @@ const personSchema = new mongoose.Schema({
     address:{
         type:String,
         required: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
     }
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const db = require('./db'); 
+require('dotenv').config();
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Person = require('./models/Person')
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -10,7 +14,33 @@ const logRequest = (req,res,next)=>{
     next();
 }
 app.use(logRequest)
-app.get('/', (req, res) => {
+
+passport.use(new LocalStrategy(async(USERNAME,password,done)=>{
+
+    try{
+          console.log('Received credentials:',USERNAME,password);
+          const user = await Person.findOne({username: USERNAME});
+          if(!user)
+          {
+              return done(null,false,{message: 'incorrect username'})
+          }
+          const isPasswordMatch = user.password == password ? true:false;
+          if(isPasswordMatch){
+            return done(null,user);
+          }
+          else
+          {
+            return done(null,false,{message: 'incorrect password'})
+          }
+    }
+    catch(err)
+    {
+         return done(err);
+    }
+}))
+passport.use(passport.initialize());
+
+app.get('/',passport.authenticate('local',{session:false}), (req, res) => {
     res.send('Welcome to my home');
 });
 
